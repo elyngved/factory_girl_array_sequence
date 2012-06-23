d96957e7493398c1b909945d19825c70d3cf4246
Erik Lyngved <elyngved@gmail.com>
2012-06-23 12:54:28 -0400

    deleting factory_girl.rb file

--- lib/factory_girl.rb
+++
@@ -1,7 +0,0 @@
-module FactoryGirl
-
-  def self.array_sequence(array)
-    FactoryGirl::ArraySequence.init(array)
-  end
-
-end
--- lib/factory_girl_array_sequence.rb
+++ lib/factory_girl_array_sequence.rb
@@ -1,3 +1,7 @@
 require "factory_girl"
 require "factory_girl_array_sequence/version"
 require "factory_girl/array_sequence"
+
+def FactoryGirl.array_sequence(array)
+  FactoryGirl::ArraySequence.init(array)
+end
